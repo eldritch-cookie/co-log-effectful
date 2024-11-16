321c76825895f91871e9ece73e663ac0e0158f72
Eldritch Cookie <eldritch.cookie@disroot.org>
2024-11-16 10:19:53 -0300

    test: mark untested and thin test import list

--- src/Effectful/Colog.hs
+++ src/Effectful/Colog.hs
@@ -29,11 +29,11 @@ import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
 import Effectful
 import Effectful.Dispatch.Static
+import Effectful.FileSystem (FileSystem)
+import Effectful.FileSystem.IO.ByteString (hPutStr)
 import Effectful.Internal.Env (Env, Relinker (..), consEnv, unconsEnv)
 import Effectful.Internal.Utils (inlineBracket)
 import Effectful.Writer.Static.Shared (Writer, runWriter, tell)
-import Effectful.FileSystem.IO.ByteString (hPutStr)
-import Effectful.FileSystem (FileSystem)
 import System.IO (Handle)
 
 -- | Provides the ability to log with an implicit 'LogEff'
@@ -67,28 +67,30 @@ runLogAction logAct act = unsafeEff $ \env -> do
 runLogWriter :: forall es msg a. (Monoid msg) => Eff (Log msg : es) a -> Eff es (a, msg)
 runLogWriter = runWriter . runLogAction tellLogEff . inject
 
-
 -- | logs a message using the implicit 'LogEff'
 logMsg :: forall msg es. (Log msg :> es) => msg -> Eff es ()
 logMsg message = do
   MkLog env act <- getStaticRep
   unsafeEff_ $ unLogEff act message env
 
+-- untested
+
 -- | converts a 'LogEff' into another compatible 'LogEff'
 injectLog :: forall (xs :: [Effect]) (es :: [Effect]) a. (Subset xs es) => LogEff xs a -> LogEff es a
 injectLog = hoistLogAction inject
 
--- untested
 -- | 'LogEff' that delegates to a static shared 'Writer' effect
-tellLogEff :: forall es msg. (Writer msg :> es,Monoid msg) => LogEff es msg
+tellLogEff :: forall es msg. (Writer msg :> es, Monoid msg) => LogEff es msg
 tellLogEff = LogAction tell
 
 -- untested
+
 -- | 'LogEff' that writes 'Text' to a 'Handle'
 textLogEff :: forall es. (FileSystem :> es) => Handle -> LogEff es Text
 textLogEff hdl = LogAction $ hPutStr hdl . encodeUtf8
 
 -- untested
+
 -- | 'LogEff' that writes 'ByteString' to a 'Handle'
 byteStringLogEff :: forall es. (FileSystem :> es) => Handle -> LogEff es ByteString
 byteStringLogEff hdl = LogAction $ hPutStr hdl
--- test/Effectful/CologTest.hs
+++ test/Effectful/CologTest.hs
@@ -1,41 +1,54 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+
 module Effectful.CologTest where
-import Data.Text(Text,pack)
---import Test.Tasty
+
+import Data.Text (Text, pack)
+
+-- import Test.Tasty
 import Test.Tasty.QuickCheck
---import Test.Tasty.HUnit
-import Effectful.Colog
+
+-- import Test.Tasty.HUnit
+
 import Effectful
+import Effectful.Colog
+import Effectful.Concurrent.Async (runConcurrent, wait, withAsync)
 import Effectful.Labeled
 import Effectful.Provider
-import Effectful.Concurrent.Async (runConcurrent, withAsync, wait)
-import Effectful.Writer.Static.Shared (runWriter, listen)
+import Effectful.Writer.Static.Shared (runWriter)
 
 instance Arbitrary Text where
   arbitrary = fmap pack arbitrary
 
 prop_tellEquals :: Text -> Property
 prop_tellEquals t = runPureEff $ do
-  (_,t2) <- runLogWriter $ logMsg t
+  (_, t2) <- runLogWriter $ logMsg t
   pure $ t === t2
 
 prop_labeledLogShared :: Text -> Property
-prop_labeledLogShared msg = property @Property
-  . runPureEff
-  . fmap ((msg ===) . snd)
-  . runLabeled runLogWriter . labeled $ logMsg msg
+prop_labeledLogShared msg =
+  property @Property
+    . runPureEff
+    . fmap ((msg ===) . snd)
+    . runLabeled runLogWriter
+    . labeled
+    $ logMsg msg
 
 prop_providerLogShared :: Text -> Property
-prop_providerLogShared msg = property @Property
-  . runPureEff
-  . fmap ((=== msg) . snd)
-  . runWriter @Text
-  . runProvider_ (runLogAction) $ provideWith_ (tellLogEff ) $ logMsg @Text msg
+prop_providerLogShared msg =
+  property @Property
+    . runPureEff
+    . fmap ((=== msg) . snd)
+    . runWriter @Text
+    . runProvider_ (runLogAction)
+    $ provideWith_ (tellLogEff)
+    $ logMsg @Text msg
 
 prop_tellConcurrent :: Text -> Text -> Property
-prop_tellConcurrent m1 m2 = ioProperty @Property 
-  . runEff 
-  . runConcurrent 
-  . fmap ((=== (m1 <> m2)) . snd) 
-  . runWriter @Text 
-  . runLogAction tellLogEff $ logMsg m1 >> withAsync (logMsg m2) (\a -> wait a >> pure ())
+prop_tellConcurrent m1 m2 =
+  ioProperty @Property
+    . runEff
+    . runConcurrent
+    . fmap ((=== (m1 <> m2)) . snd)
+    . runWriter @Text
+    . runLogAction tellLogEff
+    $ logMsg m1 >> withAsync (logMsg m2) (\a -> wait a >> pure ())
